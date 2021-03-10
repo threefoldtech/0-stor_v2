bf5a4e4690ba5d853408bff9b1e20f8def4cf9ed
Lee Smet <lee.smet@hotmail.com>
2021-03-10 18:20:40 +0100

    Add dedicated error struct for compression
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- src/compression.rs
+++ src/compression.rs
@@ -1,11 +1,16 @@
+use std::fmt;
+use std::io;
 use std::io::{copy, Cursor};
+
+/// Result type for compressor operations.
+pub type CompressorResult<T> = Result<T, CompressorError>;
 
 /// A compression unit allows compressing and later decompressing data.
 pub trait Compressor {
     /// Compress the given input buffer, returning the compressed data.
-    fn compress(&self, data: &[u8]) -> Result<Vec<u8>, String>;
+    fn compress(&self, data: &[u8]) -> CompressorResult<Vec<u8>>;
     /// Decompress the given input buffer, returning the decompressed data.
-    fn decompress(&self, data: &[u8]) -> Result<Vec<u8>, String>;
+    fn decompress(&self, data: &[u8]) -> CompressorResult<Vec<u8>>;
 }
 
 /// A compressor implementing the snappy algorithm
@@ -13,29 +18,79 @@ pub trait Compressor {
 pub struct Snappy;
 
 impl Compressor for Snappy {
-    fn compress(&self, data: &[u8]) -> Result<Vec<u8>, String> {
+    fn compress(&self, data: &[u8]) -> CompressorResult<Vec<u8>> {
         let mut input = Cursor::new(data);
         let out = Cursor::new(Vec::new());
 
         let mut wtr = snap::write::FrameEncoder::new(out);
 
-        copy(&mut input, &mut wtr).map_err(|e| format!("could not compress data: {}", e))?;
+        copy(&mut input, &mut wtr).map_err(|e| CompressorError {
+            kind: CompressorErrorKind::Compress,
+            internal: e,
+        })?;
 
         Ok(wtr
             .into_inner()
-            .map_err(|e| format!("could not recover output buffer: {}", e))?
+            .map_err(|e| CompressorError {
+                kind: CompressorErrorKind::Compress,
+                internal: io::Error::from(e.error().kind()), // TODO: this could be better
+            })?
             .into_inner())
     }
 
-    fn decompress(&self, data: &[u8]) -> Result<Vec<u8>, String> {
+    fn decompress(&self, data: &[u8]) -> CompressorResult<Vec<u8>> {
         let input = Cursor::new(data);
         let mut out = Cursor::new(Vec::new());
 
         let mut rdr = snap::read::FrameDecoder::new(input);
 
-        copy(&mut rdr, &mut out).map_err(|e| format!("could not compress data: {}", e))?;
+        copy(&mut rdr, &mut out).map_err(|e| CompressorError {
+            kind: CompressorErrorKind::Decompress,
+            internal: e,
+        })?;
 
         Ok(out.into_inner())
+    }
+}
+
+/// An error holding details about compression failure
+#[derive(Debug)]
+pub struct CompressorError {
+    kind: CompressorErrorKind,
+    internal: io::Error,
+}
+
+impl fmt::Display for CompressorError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error in {}: {}", self.kind, self.internal)
+    }
+}
+
+impl std::error::Error for CompressorError {
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        Some(&self.internal)
+    }
+}
+
+/// The cause of a compression error
+#[derive(Debug)]
+pub enum CompressorErrorKind {
+    /// Error while compressing
+    Compress,
+    /// Error while decompressing
+    Decompress,
+}
+
+impl fmt::Display for CompressorErrorKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "operation {}",
+            match self {
+                CompressorErrorKind::Compress => "COMPRESS",
+                CompressorErrorKind::Decompress => "DECOMPRESS",
+            }
+        )
     }
 }
 
