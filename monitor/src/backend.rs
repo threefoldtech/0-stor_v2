f02e13e2c0b34ad8d34ac7a00a92f43ce5c68b05
Lee Smet <lee.smet@hotmail.com>
2021-03-19 00:29:10 +0100

    Backend state detection WIP
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- monitor/src/backend.rs
+++ monitor/src/backend.rs
@@ -1,6 +1,7 @@
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
-pub struct BackendMonitor {}
+// Amount of time a backend can be unreachable before it is actually considered unreachable
+const MISSING_DURATION: Duration = Duration::from_secs(15 * 60);
 
 #[derive(Debug)]
 pub enum BackendState {
@@ -11,4 +12,24 @@ pub enum BackendState {
     Unreachable,
     // The shard reports low space, amount of space left is included in bytes
     LowSpace(usize),
+}
+
+impl BackendState {
+    pub fn is_unreachable(&mut self) {
+        match self {
+            BackendState::Unknown(since) if since.elapsed() >= MISSING_DURATION => {
+                *self = BackendState::Unreachable;
+            }
+            BackendState::Unknown(since) if since.elapsed() < MISSING_DURATION => (),
+            _ => *self = BackendState::Unknown(Instant::now()),
+        }
+    }
+
+    pub fn is_healthy(&mut self) {
+        *self = BackendState::Healthy;
+    }
+
+    pub fn is_lowspace(&mut self, left: usize) {
+        *self = BackendState::LowSpace(left);
+    }
 }
