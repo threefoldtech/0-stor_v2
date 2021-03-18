385cca3ec292f597c557f5b8f4b5cf1045621255
Lee Smet <lee.smet@hotmail.com>
2021-03-19 00:29:10 +0100

    Backend state detection WIP
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- monitor/src/lib.rs
+++ monitor/src/lib.rs
@@ -1,6 +1,7 @@
 use backend::BackendState;
 use config::Config;
-use log::{debug, error};
+use futures::future::join_all;
+use log::{debug, error, warn};
 use std::collections::HashMap;
 use std::error;
 use std::fmt;
@@ -10,10 +11,11 @@ use tokio::fs::File;
 use tokio::io::{self, AsyncReadExt};
 use tokio::select;
 use tokio::sync::mpsc::{channel, Receiver};
-use tokio::task::JoinHandle;
+use tokio::task::{JoinError, JoinHandle};
 use tokio::time::interval;
 use zstor_v2::config::Config as ZStorConfig;
-use zstor_v2::zdb::ZdbConnectionInfo;
+use zstor_v2::zdb::{Zdb, ZdbConnectionInfo, ZdbError, ZdbResult};
+use zstor_v2::{ZstorError, ZstorResult};
 
 pub mod backend;
 pub mod config;
@@ -57,17 +59,19 @@ impl Monitor {
         unimplemented!();
     }
 
-    pub async fn monitor_backends(&self, mut rx: Receiver<()>) -> JoinHandle<()> {
+    pub async fn monitor_backends(&self, mut rx: Receiver<()>) -> JoinHandle<MonitorResult<()>> {
         let config = self.cfg.clone();
-        let mut backends = HashMap::<ZdbConnectionInfo, BackendState>::new();
+
         tokio::spawn(async move {
             let mut ticker = interval(BACKEND_MONITOR_INTERVAL);
+            let mut backends = HashMap::<ZdbConnectionInfo, BackendState>::new();
             loop {
                 select! {
                     _ = rx.recv() => {
-                        return
+                        return Ok(())
                     }
                     _ = ticker.tick() => {
+                        debug!("reading zstor config at {:?}", config.zstor_config_path());
                         let zstor_config = match read_zstor_config(config.zstor_config_path()).await {
                             Ok(cfg) => cfg,
                             Err(e) => {
@@ -75,19 +79,55 @@ impl Monitor {
                                 continue
                             }
                         };
-                        for backend in zstor_config.backends() {
-                            backends.entry(backend.clone()).or_insert(BackendState::Unknown(std::time::Instant::now()));
-                        }
 
-                        for backend_group in backends.keys().collect::<Vec<_>>().chunks(MAX_CONCURRENT_CONNECTIONS) {
-                            let mut futs = Vec::new(); // JOINHANDLES
+                       for backend in zstor_config.backends() {
+                           backends
+                               .entry(backend.clone())
+                               .or_insert(BackendState::Unknown(std::time::Instant::now()));
+                       }
+
+                        let keys = backends.keys().into_iter().cloned().collect::<Vec<_>>();
+                        for backend_group in keys.chunks(MAX_CONCURRENT_CONNECTIONS) {
+                            let mut futs: Vec<JoinHandle<Result<_,(_,ZstorError)>>> = Vec::with_capacity(backend_group.len());
                             for backend in backend_group {
+                                let backend = backend.clone();
+                                futs.push(tokio::spawn(async move{
+                                    // connect to backend and get size
+                                    let ns_info = Zdb::new(backend.clone())
+                                        .await
+                                        .map_err(|e| (backend.clone(), e.into()))?
+                                        .ns_info()
+                                        .await
+                                        .map_err(|e| (backend.clone(), e.into()))?;
 
+                                    Ok((backend, ns_info.free_space()))
+
+                                }));
+                            }
+                            for result in join_all(futs).await {
+                                // TODO: decide when free space is insufficient
+                                let (backend, free_space) = match result? {
+                                    Ok(succes) => succes,
+                                    Err((backend, e)) => {
+                                        warn!("backend {} can not be reached {}", backend.address(), e);
+                                        backends.entry(backend).and_modify(BackendState::is_unreachable);
+                                        continue;
+                                    }
+                                };
+
+                                // TODO:
+                                let treshold = 1000000;
+                                if free_space < treshold {
+                                    warn!("backend {} is low on space ({} < {})", backend.address(), free_space, treshold);
+                                    backends.entry(backend).and_modify(|bs| bs.is_lowspace(free_space));
+                                } else {
+                                    backends.entry(backend).and_modify(BackendState::is_healthy);
+                                }
                             }
                         }
 
-                        // Connect to backends and check state
                         // TODO
+                        // Repair data containing broken backends
                     }
                 }
             }
@@ -128,6 +168,8 @@ impl error::Error for MonitorError {
         match self.internal {
             InternalError::IO(ref e) => Some(e),
             InternalError::Format(ref e) => Some(e),
+            InternalError::Task(ref e) => Some(e),
+            InternalError::Zstor(ref e) => Some(e),
         }
     }
 }
@@ -141,9 +183,29 @@ impl MonitorError {
     }
 }
 
+impl From<JoinError> for MonitorError {
+    fn from(e: JoinError) -> Self {
+        MonitorError {
+            kind: ErrorKind::Task,
+            internal: InternalError::Task(e),
+        }
+    }
+}
+
+impl From<ZstorError> for MonitorError {
+    fn from(e: ZstorError) -> Self {
+        MonitorError {
+            kind: ErrorKind::Zstor,
+            internal: InternalError::Zstor(e),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum ErrorKind {
     Config,
+    Task,
+    Zstor,
 }
 
 impl fmt::Display for ErrorKind {
@@ -153,6 +215,8 @@ impl fmt::Display for ErrorKind {
             "{}",
             match self {
                 ErrorKind::Config => "Config",
+                ErrorKind::Task => "Unrecoverable failure in asynchronous task",
+                ErrorKind::Zstor => "0-stor operation error",
             }
         )
     }
@@ -162,6 +226,8 @@ impl fmt::Display for ErrorKind {
 pub enum InternalError {
     IO(io::Error),
     Format(toml::de::Error),
+    Task(tokio::task::JoinError),
+    Zstor(ZstorError),
 }
 
 impl fmt::Display for InternalError {
@@ -172,6 +238,8 @@ impl fmt::Display for InternalError {
             match self {
                 InternalError::IO(ref e) => e as &dyn fmt::Display,
                 InternalError::Format(ref e) => e,
+                InternalError::Task(ref e) => e,
+                InternalError::Zstor(ref e) => e,
             }
         )
     }
