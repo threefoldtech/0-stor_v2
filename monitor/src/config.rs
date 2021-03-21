fc4250bb7bf30c92d177a0651a2a555404ed9022
Lee Smet <lee.smet@hotmail.com>
2021-03-21 01:14:06 +0100

    Make ns filled treshold configurable
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- monitor/src/config.rs
+++ monitor/src/config.rs
@@ -8,10 +8,12 @@ pub struct Config {
     zdb_data_dir_path: PathBuf,
     zstor_config_path: PathBuf,
     zstor_bin_path: PathBuf,
-    vdc_config: Option<VdcConfig>,
     /// Maximum size of the data dir in MiB, if this is set and the sum of the file sizes in the
     /// data dir gets higher than this value, the least used, already encoded file will be removed.
     max_zdb_data_dir_size: Option<usize>,
+    #[serde(default = "default_ns_filled_treshold")]
+    zdb_namespace_fill_treshold: u8,
+    vdc_config: Option<VdcConfig>,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -47,6 +49,10 @@ impl Config {
     pub fn max_zdb_data_dir_size(&self) -> Option<usize> {
         self.max_zdb_data_dir_size
     }
+
+    pub fn zdb_namespace_fill_treshold(&self) -> u8 {
+        self.zdb_namespace_fill_treshold
+    }
 }
 
 impl VdcConfig {
@@ -60,5 +66,86 @@ impl VdcConfig {
 
     pub fn new_size(&self) -> usize {
         self.new_size
+    }
+}
+
+fn default_ns_filled_treshold() -> u8 {
+    95
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Config, VdcConfig};
+
+    #[test]
+    fn test_serialize_config() {
+        let config = Config {
+            zdb_index_dir_path: "/tmp/zdb/zdb-index".parse().unwrap(),
+            zdb_data_dir_path: "/tmp/zdb/zdb-index".parse().unwrap(),
+            zstor_config_path: "/tmp/zstor_config.toml".parse().unwrap(),
+            zstor_bin_path: "/tmp/zstor_v2".parse().unwrap(),
+            max_zdb_data_dir_size: Some(1024 * 50), // 50 GiB
+            zdb_namespace_fill_treshold: 90,        // 90% before rotation
+            vdc_config: Some(VdcConfig {
+                url: "https://some.evdc.tech".to_string(),
+                password: "supersecurepassword".to_string(),
+                new_size: 20, // 20 GB
+            }),
+        };
+
+        let output = toml::to_string(&config).unwrap();
+
+        // let expected = r#""zdb_index_dir_path = \"/tmp/zdb/zdb-index\"\nzdb_data_dir_path = \"/tmp/zdb/zdb-index\"\nzstor_config_path = \"/tmp/zstor_config.toml\"\nzstor_bin_path = \"/tmp/zstor_v2\"\nmax_zdb_data_dir_size = 51200\nzdb_namespace_fill_treshold = 90\n\n[vdc_config]\nurl = \"https://some.evdc.tech\"\npassword = \"supersecurepassword\"\nnew_size = 20\n"#;
+        let expected = r#"zdb_index_dir_path = "/tmp/zdb/zdb-index"
+zdb_data_dir_path = "/tmp/zdb/zdb-index"
+zstor_config_path = "/tmp/zstor_config.toml"
+zstor_bin_path = "/tmp/zstor_v2"
+max_zdb_data_dir_size = 51200
+zdb_namespace_fill_treshold = 90
+
+[vdc_config]
+url = "https://some.evdc.tech"
+password = "supersecurepassword"
+new_size = 20
+"#;
+
+        println!("{}", output);
+
+        assert_eq!(&output, expected);
+    }
+
+    #[test]
+    fn test_deserialize_config() {
+        let input = r#"
+zdb_index_dir_path = "/tmp/zdb/zdb-index"
+zdb_data_dir_path = "/tmp/zdb/zdb-index"
+zstor_config_path = "/tmp/zstor_config.toml"
+zstor_bin_path = "/tmp/zstor_v2"
+max_zdb_data_dir_size = 51200
+zdb_namespace_fill_treshold = 90
+
+[vdc_config]
+url = "https://some.evdc.tech"
+password = "supersecurepassword"
+new_size = 20
+"#;
+
+        let config = toml::from_str(input).unwrap();
+
+        let output = Config {
+            zdb_index_dir_path: "/tmp/zdb/zdb-index".parse().unwrap(),
+            zdb_data_dir_path: "/tmp/zdb/zdb-index".parse().unwrap(),
+            zstor_config_path: "/tmp/zstor_config.toml".parse().unwrap(),
+            zstor_bin_path: "/tmp/zstor_v2".parse().unwrap(),
+            max_zdb_data_dir_size: Some(1024 * 50), // 50 GiB
+            zdb_namespace_fill_treshold: 90,        // 90% before rotation
+            vdc_config: Some(VdcConfig {
+                url: "https://some.evdc.tech".to_string(),
+                password: "supersecurepassword".to_string(),
+                new_size: 20, // 20 GB
+            }),
+        };
+
+        assert_eq!(output, config);
     }
 }
