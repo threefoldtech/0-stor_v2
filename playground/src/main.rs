5975f4f371effe075856fe2f444a3eecf482e1e4
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-03-30 11:03:17 +0200

    WIP add pool get and create

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -11,12 +11,16 @@ fn main() {
         // nodes_get().await;
         // farms_get().await;
         // farm_get().await;
-        workload_get().await;
+        // workload_get().await;
+        pool_get().await;
     });
 }
 
+const SECRET: &str = "SBN3N6R2FSIHJTZWSVBQAVUJO4LGQCYFWNWZ3DUG5VMVIYHZIZXWGW3O";
+const USER_ID: i64 = 1;
+
 async fn node_get() {
-    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string());
+    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string(), SECRET, USER_ID);
     let result = client.node_get_by_id("2anfZwrzskXiUHPLTqH1veJAia8G6rW2eFLy5YFMa1MP".to_string()).await;
     match result {
         Ok(node) => {
@@ -29,7 +33,7 @@ async fn node_get() {
 } 
 
 async fn nodes_get() {
-    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string());
+    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string(), SECRET, USER_ID);
     let result = client.nodes_get().await;
     match result {
         Ok(nodes) => {
@@ -42,7 +46,7 @@ async fn nodes_get() {
 } 
 
 async fn farm_get() {
-    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string());
+    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string(), SECRET, USER_ID);
     let result = client.farm_get_by_id(1).await;
     match result {
         Ok(node) => {
@@ -55,7 +59,7 @@ async fn farm_get() {
 } 
 
 async fn farms_get() {
-    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string());
+    let client = grid_explorer_client::new_explorer_client("https://explorer.devnet.grid.tf".to_string(), SECRET, USER_ID);
     let result = client.farms_get().await;
     match result {
         Ok(nodes) => {
@@ -68,11 +72,24 @@ async fn farms_get() {
 }
 
 async fn workload_get() {
-    let client = grid_explorer_client::new_explorer_client("https://explorer.testnet.grid.tf".to_string());
+    let client = grid_explorer_client::new_explorer_client("https://explorer.testnet.grid.tf".to_string(), SECRET, USER_ID);
     let result = client.workload_get_by_id(28338).await;
     match result {
         Ok(workload) => {
             println!("{:?}", workload);
+        },
+        Err(err) => {
+            println!("{:?}", err)
+        }
+    }
+}
+
+async fn pool_get() {
+    let client = grid_explorer_client::new_explorer_client("https://explorer.testnet.grid.tf".to_string(), SECRET, USER_ID);
+    let result = client.pool_get_by_id(2).await;
+    match result {
+        Ok(pool) => {
+            println!("{:?}", pool);
         },
         Err(err) => {
             println!("{:?}", err)
