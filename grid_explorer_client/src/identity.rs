5d080280bd466a37afde490cff6b4c2ad465c3fa
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-03-31 11:29:28 +0200

    add wip

--- grid_explorer_client/src/identity.rs
+++ grid_explorer_client/src/identity.rs
@@ -1,4 +1,8 @@
-use stellar_base::crypto::KeyPair;
+// use stellar_base::crypto::KeyPair;
+use bip39::{Mnemonic};
+use std::str::FromStr;
+use ed25519_dalek::{Signer, Keypair};
+use hex;
 
 pub struct Identity {
     pub name: String,
@@ -12,6 +16,18 @@ impl Identity {
     }
 
     pub fn sign_hex(&self, json: String) -> String {
-        unimplemented!()
+        let mnemonic = Mnemonic::from_str("").unwrap();
+
+        let seed = mnemonic.to_entropy();
+
+        // if seed.len() != 32 {
+            // throw error
+        // }
+
+        let keypair: Keypair = Keypair::from_bytes(&seed[0..31]).unwrap();
+
+        let signature = keypair.sign(json.as_bytes());
+
+        hex::encode(signature.to_bytes().to_vec())
     }
 }
