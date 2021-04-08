77d4bcf06f7bc91d948a1078e47da14834436da5
dylanVerstraete <dylan_verstraete1@hotmail.com>
2021-04-08 16:52:08 +0200

    refactors and add pool creation + payment

--- grid_explorer_client/src/identity.rs
+++ grid_explorer_client/src/identity.rs
@@ -1,13 +1,54 @@
 // use stellar_base::crypto::KeyPair;
-use bip39::{Mnemonic};
+use bip39::{Mnemonic, Error};
 use std::str::FromStr;
-use ed25519_dalek::{Signer, Keypair};
+use ed25519_dalek::{Signer, Keypair, SecretKey, PublicKey, SignatureError};
 use hex;
+
+#[derive(Debug)]
+pub enum IdentityError {
+    MnemonicError(Error),
+    SignatureError(SignatureError),
+}
+
+impl From<Error> for IdentityError {
+    fn from(err: Error) -> IdentityError {
+        IdentityError::MnemonicError(err)
+    }
+}
+
+impl From<SignatureError> for IdentityError {
+    fn from(err: SignatureError) -> IdentityError {
+        IdentityError::SignatureError(err)
+    }
+}
 
 pub struct Identity {
     pub name: String,
     pub email: String,
     pub user_id: i64,
+    pub keypair: Keypair,
+}
+
+pub fn new(name: String, email: String, user_id: i64, mnemonic: &str) -> Result<Identity, IdentityError> {
+    let mnemonic = Mnemonic::from_str(mnemonic)?;
+    let entropy = mnemonic.to_entropy();
+
+    let secret_key: SecretKey = SecretKey::from_bytes(&entropy)?;
+    let public_key: PublicKey = (&secret_key).into();
+
+    let keypair = Keypair {
+        secret: secret_key,
+        public: public_key
+    };
+
+    let id = Identity{
+        name,
+        email,
+        user_id,
+        keypair
+    };
+
+    return Ok(id)
 }
 
 impl Identity {
@@ -16,18 +57,6 @@ impl Identity {
     }
 
     pub fn sign_hex(&self, json: String) -> String {
-        let mnemonic = Mnemonic::from_str("").unwrap();
-
-        let seed = mnemonic.to_entropy();
-
-        // if seed.len() != 32 {
-            // throw error
-        // }
-
-        let keypair: Keypair = Keypair::from_bytes(&seed[0..31]).unwrap();
-
-        let signature = keypair.sign(json.as_bytes());
-
-        hex::encode(signature.to_bytes().to_vec())
+        hex::encode(self.keypair.sign(json.as_bytes()).to_bytes().to_vec())
     }
 }
