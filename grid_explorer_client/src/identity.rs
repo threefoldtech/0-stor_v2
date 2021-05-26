6b2781c9c9fb7ba4b8326cfaf55747f55752efab
Omar Elawady <omarelawady1998@gmail.com>
2021-05-26 10:47:53 +0200

    add new to implementation block

--- grid_explorer_client/src/identity.rs
+++ grid_explorer_client/src/identity.rs
@@ -1,4 +1,3 @@
-// use stellar_base::crypto::KeyPair;
 use bip39::{Mnemonic, Error};
 use std::str::FromStr;
 use ed25519_dalek::{Signer, Keypair, SecretKey, PublicKey, SignatureError};
@@ -29,29 +28,29 @@ pub struct Identity {
     pub keypair: Keypair,
 }
 
-pub fn new(name: String, email: String, user_id: i64, mnemonic: &str) -> Result<Identity, IdentityError> {
-    let mnemonic = Mnemonic::from_str(mnemonic)?;
-    let entropy = mnemonic.to_entropy();
-
-    let secret_key: SecretKey = SecretKey::from_bytes(&entropy)?;
-    let public_key: PublicKey = (&secret_key).into();
-
-    let keypair = Keypair {
-        secret: secret_key,
-        public: public_key
-    };
-
-    let id = Identity{
-        name,
-        email,
-        user_id,
-        keypair
-    };
-
-    return Ok(id)
-}
 
 impl Identity {
+    pub fn new(name: String, email: String, user_id: i64, mnemonic: &str) -> Result<Identity, IdentityError> {
+        let mnemonic = Mnemonic::from_str(mnemonic)?;
+        let entropy = mnemonic.to_entropy();
+    
+        let secret_key: SecretKey = SecretKey::from_bytes(&entropy)?;
+        let public_key: PublicKey = (&secret_key).into();
+    
+        let keypair = Keypair {
+            secret: secret_key,
+            public: public_key
+        };
+    
+        let id = Identity{
+            name,
+            email,
+            user_id,
+            keypair
+        };
+    
+        Ok(id)
+    }
     pub fn get_id(&self) -> i64 {
         self.user_id
     }
