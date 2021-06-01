10dbfffb70ee66f476917b8e17f2ece7dfc8ae86
Lee Smet <lee.smet@hotmail.com>
2021-06-01 16:30:02 +0200

    Add config actor
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ zstor/src/actors/config.rs
@@ -0,0 +1,54 @@
+use crate::{config::Config, ZstorError};
+use actix::prelude::*;
+use std::path::PathBuf;
+use std::sync::Arc;
+use tokio::fs;
+
+#[derive(Message)]
+#[rtype(result = "Arc<Config>")]
+/// Message to request the current config
+pub struct GetConfig;
+
+#[derive(Message)]
+#[rtype(result = "Result<(), ZstorError>")]
+/// Message to trigger a config reload
+pub struct ReloadConfig;
+
+/// Actor holding the configuration, and the path where it is saved.
+pub struct ConfigActor {
+    config_path: PathBuf,
+    config: Arc<Config>,
+}
+
+impl Actor for ConfigActor {
+    type Context = Context<Self>;
+}
+
+impl Handler<GetConfig> for ConfigActor {
+    type Result = Arc<Config>;
+
+    fn handle(&mut self, _: GetConfig, _: &mut Self::Context) -> Self::Result {
+        self.config.clone()
+    }
+}
+
+impl Handler<ReloadConfig> for ConfigActor {
+    type Result = AtomicResponse<Self, Result<(), ZstorError>>;
+
+    fn handle(&mut self, _: ReloadConfig, _: &mut Self::Context) -> Self::Result {
+        let path = self.config_path.clone();
+        AtomicResponse::new(Box::pin(
+            async move {
+                fs::read(path)
+                    .await
+                    .map_err(|e| ZstorError::new_io("Couldn't load config file".to_string(), e))
+            }
+            .into_actor(self)
+            .map(|res, this, _| {
+                let config = toml::from_slice(&res?)?;
+                this.config = Arc::new(config);
+                Ok(())
+            }),
+        ))
+    }
+}
