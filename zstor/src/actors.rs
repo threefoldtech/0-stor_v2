b839805ecf0fa0b29816a3ac23f045b73ecdc4c9
Lee Smet <lee.smet@hotmail.com>
2021-06-02 15:22:30 +0200

    Add MetaStore actor implementation
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- zstor/src/actors.rs
+++ zstor/src/actors.rs
@@ -1,4 +1,6 @@
 /// Actor implementation of the configuration manager
 pub mod config;
+/// Actor implementation of the [`MetaStore`](crate::meta::MetaStore)
+pub mod meta;
 /// Actor implementation of the pipeline
 pub mod pipeline;
