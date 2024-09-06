86dbface84616ecf828dd840ca3e6a0a85a4a766
Lee Smet <lee.smet@hotmail.com>
2024-09-06 10:48:36 +0200

    Remove legacy(?) monitor code
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- monitor/src/monitors.rs
+++
@@ -1,7 +0,0 @@
-pub mod backends;
-pub mod failed_writes;
-pub mod ns_datasize;
-
-pub use backends::monitor_backends;
-pub use failed_writes::monitor_failed_writes;
-pub use ns_datasize::monitor_ns_datasize;
